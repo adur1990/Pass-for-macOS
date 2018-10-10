417f8242f9e588c7265f521c176f50d0833d418b
Artur <git@artursterz.de>
2018-10-10 12:35:59 +0200

    Initial Commit

---
+++ passafari/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  passafari
+//
+//  Created by Artur Sterz on 10.10.18.
+//  Copyright © 2018 Artur Sterz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ passafari/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  passafari
+//
+//  Created by Artur Sterz on 10.10.18.
+//  Copyright © 2018 Artur Sterz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
