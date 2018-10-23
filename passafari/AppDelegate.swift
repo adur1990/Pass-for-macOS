d982d65f6c3c5fbdfb5b646d8679209ba5cb22ce
Artur <git@artursterz.de>
2018-10-23 19:09:05 +0200

    Implement sandboxed file access
    For allowing passafari to access files outside of the sandbox, we need to ask the user for permission.
    Furthermore, this permission also needs to be persisted for future launches

--- passafari/AppDelegate.swift
+++ passafari/AppDelegate.swift
@@ -12,92 +12,11 @@ import SafariServices
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    let center = DistributedNotificationCenter.default()
-    let FUZZY_SEARCH_SCORE = 0.3
-    
-    /*
-     We need the path to the default password store. As of writing this, we only accept the default path, which is $HOME/.password-store/
-    */
-    private var passwordStoreUrl: NSURL? {
-        let pw = getpwuid(getuid())
-        let home = pw?.pointee.pw_dir
-        let homePath = FileManager.default.string(withFileSystemRepresentation: home!, length: Int(strlen(home)))
-        let homePathUrl = NSURL(string: "\(homePath)/.password-store/")
-        
-        return homePathUrl
-    }
-    
-    /*
-     The GPG key ID needed for decrypting the password file.
-    */
-    private var gpgKeyId: String {
-        do {
-            return try String(contentsOfFile: passwordStoreUrl!.appendingPathComponent(".gpg-id")!.absoluteString, encoding: .utf8)
-        }
-        catch {
-            return "nil"
-        }
-    }
-    
-    /*
-     Main password search function. This will be used for searching passwords from the default store path.
-     We use a fuzzy search in all cases.
-    */
-    func passSearch(password: String) -> [String] {
-        var resultPaths = [String]()
-        
-        let fileSystem = FileManager.default
-        if let fsTree = fileSystem.enumerator(atPath: passwordStoreUrl!.absoluteString!) {
-            
-            // Iterate over all files and folders in the default store path. Several checks will be done.
-            while let fsNodeName = fsTree.nextObject() as? String {
-                let fullPath = "\(String(describing: passwordStoreUrl!))\(fsNodeName)"
-                var pathComponents = URL(fileURLWithPath: fullPath).pathComponents
-                
-                // To not accidentally exclude a valid encrypted file from the search path, we remove it beforehand from the .git search components.
-                if pathComponents.last!.hasSuffix(".gpg") {
-                    pathComponents.removeLast()
-                } else {
-                    continue
-                }
-                
-                // If a password store is a git folder, we do not want to iterate over the entire repository, but only the current working copy.
-                // Therefore, we need to exclude all folders containing .git from the search items.
-                if pathComponents.contains(where:
-                    { (pathComponent) -> Bool in
-                        return pathComponent.contains(".git")
-                    }) {
-                    continue
-                }
-                
-                // The final scoring. If a path matches at least FUZZY_SEARCH_SCORE, we remember it.
-                if FuzzySearch.score(originalString:fullPath, stringToMatch: password) > FUZZY_SEARCH_SCORE {
-                    resultPaths.append(fsNodeName)
-                }
-            }
-        }
-        
-        return resultPaths
-    }
-    
-    @objc func handleMessageFromContainingApp(notif: Notification) {
-        var foundPasswords = passSearch(password: notif.object as! String)
-        if foundPasswords.isEmpty {
-            foundPasswords = ["No matching password found."]
-        }
-        print(foundPasswords)
-    }
-    
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        center.addObserver(self, selector: #selector(self.handleMessageFromContainingApp(notif:)), name: Notification.Name("search"), object: nil)
         // Insert code here to tear down your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
--- passafari/ViewController.swift
+++ passafari/ViewController.swift
@@ -9,11 +9,114 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    let center = DistributedNotificationCenter.default()
+    let FUZZY_SEARCH_SCORE = 0.3
+    
+    /*
+     Main password search function. This will be used for searching passwords from the default store path.
+     We use a fuzzy search in all cases.
+     */
+    func passSearch(password: String) -> [String] {
+        var resultPaths = [String]()
+        
+        let passwordStoreUrl = getPathFromBookmark()
+        
+        if passwordStoreUrl!.startAccessingSecurityScopedResource() {
+            let fileSystem = FileManager.default
+            if let fsTree = fileSystem.enumerator(atPath: passwordStoreUrl!.path) {
+                // Iterate over all files and folders in the default store path. Several checks will be done.
+                while let fsNodeName = fsTree.nextObject() as? String {
+                    let fullPath = "\(String(describing: passwordStoreUrl!))\(fsNodeName)"
+                    var pathComponents = URL(fileURLWithPath: fullPath).pathComponents
+                    
+                    // To not accidentally exclude a valid encrypted file from the search path, we remove it beforehand from the .git search components.
+                    if pathComponents.last!.hasSuffix(".gpg") {
+                        pathComponents.removeLast()
+                    } else {
+                        continue
+                    }
+                    
+                    // If a password store is a git folder, we do not want to iterate over the entire repository, but only the current working copy.
+                    // Therefore, we need to exclude all folders containing .git from the search items.
+                    if pathComponents.contains(where:
+                        { (pathComponent) -> Bool in
+                            return pathComponent.contains(".git")
+                    }) {
+                        continue
+                    }
+                    
+                    // The final scoring. If a path matches at least FUZZY_SEARCH_SCORE, we remember it.
+                    if FuzzySearch.score(originalString:fullPath, stringToMatch: password) > FUZZY_SEARCH_SCORE {
+                        resultPaths.append(fsNodeName)
+                    }
+                }
+            }
+        }
+        
+        passwordStoreUrl!.stopAccessingSecurityScopedResource()
+        
+        return resultPaths
+    }
+    
+    @objc func handleMessageFromContainingApp(notif: Notification) {
+        var foundPasswords = passSearch(password: notif.object as! String)
+        if foundPasswords.isEmpty {
+            foundPasswords = ["No matching password found."]
+        }
+        print(foundPasswords)
+    }
+    
+    func promptForPath() -> URL? {
+        let openPanel = NSOpenPanel()
+        openPanel.directoryURL = FileManager.default.homeDirectoryForCurrentUser
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.canCreateDirectories = false
+        openPanel.allowsMultipleSelection = false
+        openPanel.showsHiddenFiles = true
+        openPanel.title = "Select password-store default store"
+        
+        if openPanel.runModal() == NSApplication.ModalResponse.OK && !openPanel.urls.isEmpty {
+            return openPanel.url
+        }
+        
+        return nil
+    }
+    
+    func getPathFromBookmark() -> URL? {
+        if let data = UserDefaults.standard.data(forKey: "passPathBookmark") {
+            var bookmarkDataIsStale: ObjCBool = false
+            
+            let url = try! (NSURL(resolvingBookmarkData: data, options: [.withoutUI, .withSecurityScope], relativeTo: nil, bookmarkDataIsStale: &bookmarkDataIsStale) as URL)
+            if bookmarkDataIsStale.boolValue {
+                print("WARNING stale security bookmark")
+                return nil
+            }
+            
+            return url
+        }
+        
+        return nil
+    }
+    
+    func savePathToBookmark(url: URL) {
+        let data = try! url.bookmarkData(options: .withSecurityScope, includingResourceValuesForKeys: nil, relativeTo: nil)
+        UserDefaults.standard.set(data, forKey: "passPathBookmark")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        center.addObserver(self, selector: #selector(self.handleMessageFromContainingApp(notif:)), name: Notification.Name("search"), object: nil)
+        
+        let urlFromBookmark = getPathFromBookmark()
+        
+        if urlFromBookmark == nil {
+            if let urlFromPanel = promptForPath() {
+                savePathToBookmark(url: urlFromPanel)
+            }
+        }
 
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
