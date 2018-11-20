0154018718b6f2c61082da8cfb791e09f0b62db8
Artur <git@artursterz.de>
2018-11-20 19:45:01 +0100

    Add first simple status bar

--- passafari/AppDelegate.swift
+++ passafari/AppDelegate.swift
@@ -14,9 +14,21 @@ var passwordstore: Passwordstore? = nil
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
+    var statusBarItem : NSStatusItem? = nil
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let _ = ServerHandler()
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusBarItem?.button?.image = NSImage(named: "statusIcon")
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quitApp), keyEquivalent: ""))
+        statusBarItem?.menu = menu
+    }
+    
+    @objc func quitApp() {
+        NSApplication.shared.terminate(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
