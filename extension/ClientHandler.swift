24a94154be80db0b4e6331f3ccddd82bddaeb269
Artur <git@artursterz.de>
2018-12-06 19:52:42 +0100

    Increase timeouts for communication between extension and host app
    The user needs more time to be able to enter a passphrase, if needed

--- extension/ClientHandler.swift
+++ extension/ClientHandler.swift
@@ -14,7 +14,8 @@ class ClientHandler {
     func searchPasswords(searchString: String) -> [String]? {
         let data: CFData = CFDataCreate(nil, searchString, searchString.count)
         let messageID: sint32 = 0x1
-        let timeout: CFTimeInterval = 1
+        let sendTimeout: CFTimeInterval = 5
+        let recvTimeout: CFTimeInterval = 60
         let remotePort: CFMessagePort? = CFMessagePortCreateRemote(nil, "BR355MFMD5.de.artursterz.passafari.messageport" as CFString)
         let returnDataPtr: UnsafeMutablePointer<Unmanaged<CFData>?> = UnsafeMutablePointer.allocate(capacity: 1)
         defer { returnDataPtr.deallocate() }
@@ -23,7 +24,7 @@ class ClientHandler {
             return ["The Passafari app is not running."]
         }
         
-        let status: sint32 = CFMessagePortSendRequest(remotePort, messageID, data, timeout, timeout, CFRunLoopMode.defaultMode.rawValue, returnDataPtr)
+        let status: sint32 = CFMessagePortSendRequest(remotePort, messageID, data, sendTimeout, recvTimeout, CFRunLoopMode.defaultMode.rawValue, returnDataPtr)
         
         if (status == kCFMessagePortSuccess) {
             let receivedData = returnDataPtr.pointee!.takeUnretainedValue() as Data
@@ -38,12 +39,13 @@ class ClientHandler {
     func getPassword(passwordFile: String) -> String? {
         let data: CFData = CFDataCreate(nil, passwordFile, passwordFile.count)
         let messageID: sint32 = 0x2
-        let timeout: CFTimeInterval = 1
+        let sendTimeout: CFTimeInterval = 5
+        let recvTimeout: CFTimeInterval = 60
         let remotePort: CFMessagePort = CFMessagePortCreateRemote(nil, "BR355MFMD5.de.artursterz.passafari.messageport" as CFString)
         let returnDataPtr: UnsafeMutablePointer<Unmanaged<CFData>?> = UnsafeMutablePointer.allocate(capacity: 1)
         defer { returnDataPtr.deallocate() }
         
-        let status: sint32 = CFMessagePortSendRequest(remotePort, messageID, data, timeout, timeout, CFRunLoopMode.defaultMode.rawValue, returnDataPtr)
+        let status: sint32 = CFMessagePortSendRequest(remotePort, messageID, data, sendTimeout, recvTimeout, CFRunLoopMode.defaultMode.rawValue, returnDataPtr)
         
         if (status == kCFMessagePortSuccess) {
             let receivedData = returnDataPtr.pointee!.takeUnretainedValue() as Data
---
+++ passafari/UI/ViewControllers/PassPromptViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PassPromptViewController.swift
+//  passafari
+//
+//  Created by Artur Sterz on 05.12.18.
+//  Copyright © 2018 Artur Sterz. All rights reserved.
+//
+
+import Cocoa
+
+class PassPromptViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
